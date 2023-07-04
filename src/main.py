40cf19307bbb14d407380f35bb2ec45b1c1547e9
Iurymartins46 <martinsiury46@gmail.com>
2023-07-04 19:34:53 -0300

    :test_tube: test: Criando novos casos de  testes

--- src/main.py
+++ src/main.py
@@ -4,14 +4,9 @@ import random
 expressaoRegularPadrao = r'^\w'
 
 def main():
-    """escolha = None # Para escolher qual tipo de maquina: 1 = de moore; 2 = AFD
-    while escolha not in [1, 2]:
-        print("\nQual tipo de maquina deseja ultilizar:\n1 para moore\n2 para AFD")
-        escolha = int(input("Qual sua escolha: "))"""
-    #maquina1 = funcoes.leituraArquivo('../teste/maquinaMoore01.txt')
-    maquina2 = funcoes.leituraArquivo('../teste/maquinaAFD01.txt')
-    maquina1 = funcoes.leituraArquivo('../teste/maquinaAFD02.txt')
-    #maquina2 = funcoes.leituraArquivo('../teste/maquinaAFD02.txt')
+
+    maquina1 = funcoes.leituraArquivo('../teste/AFD_10.txt')
+    maquina2 = funcoes.leituraArquivo('../teste/AFD_22.txt')
     #funcoes.jogar(maquina1, maquina2, escolha)
 
     funcoes.executaBatalha(maquina1, maquina2)
